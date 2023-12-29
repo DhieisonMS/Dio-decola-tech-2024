a9d83f6c6966d683d96406c9ca4f0f9fdeaaaeb5
DhieisonMS <jhasonx@gmail.com>
2023-12-29 20:46:36 -0300

    fazendo segundo exercicio

---
+++ desafios-codigo/explorandoSintaxe/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    string registroEmail = Console.ReadLine();
+    string registroNome = Console.ReadLine();
+    string registroAno  =  Console.ReadLine();
+    
+    Console.WriteLine($"{registroNome} verifique o email:{registroEmail} para ativar.");
+
+
+    }
+}
