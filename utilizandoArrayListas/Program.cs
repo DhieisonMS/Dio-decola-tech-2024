54034db055c10b1095053c84bd41e154f429bd9c
DhieisonMS <jhasonx@gmail.com>
2024-01-05 23:11:23 -0300

    finalizando exercicio 4

---
+++ utilizandoArrayListas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int quantidadeJogos = int.Parse(Console.ReadLine());
+        string [] jogos = new string[quantidadeJogos];
+
+        for (int i = 0; i < jogos.Length; i++ )
+        {
+            AdicionarJogo(i, jogos);
+        }
+
+        ExibirResumoAdicaoJogos(quantidadeJogos, jogos);
+    }
+
+    static void AdicionarJogo(int indice, string[] nomes)
+    {
+        nomes[indice] = Console.ReadLine();
+    }
+
+    static void ExibirResumoAdicaoJogos(int quantidadeJogos, string[] nomes)
+    {
+        string jogos = string.Join(", ", nomes);
+        Console.WriteLine($"Foi adicionado '{quantidadeJogos}' jogos: {jogos} ao catalogo.");
+    }
+}
